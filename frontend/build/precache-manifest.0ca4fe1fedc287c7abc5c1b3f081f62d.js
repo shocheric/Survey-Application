eab8408c695847fe2fa992e303ac871523cf3a67
shocheric <shocheric@gmail.com>
2024-01-10 21:05:22 -0500

    follow up commit

---
+++ frontend/build/precache-manifest.0ca4fe1fedc287c7abc5c1b3f081f62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ff9f1d9a72855a7629",
+    "url": "/static/css/main.a473495d.chunk.css"
+  },
+  {
+    "revision": "f3ff9f1d9a72855a7629",
+    "url": "/static/js/main.e10ff023.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d68e2b6e2c9ef48a7232",
+    "url": "/static/js/2.1d6f7e5c.chunk.js"
+  },
+  {
+    "revision": "cdc13a1482e1cc37d83ebe613b19fe61",
+    "url": "/static/media/lapel_shield.cdc13a14.png"
+  },
+  {
+    "revision": "25c0c6b37a110720b2206f1174b5739e",
+    "url": "/index.html"
+  }
+];
